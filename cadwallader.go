7cc65131ec3d3fdeb8fa4eaf536655a2903c30f3
Bimz <trianglehasfoursides@proton.me>
2025-08-14 15:10:34 +0700

    lgtm

--- cadwallader.go
+++ cadwallader.go
@@ -1,20 +1,15 @@
 package cadwallader
 
 import (
-	"crypto/rsa"
-	"encoding/base64"
-	"encoding/json"
-	"fmt"
-	"math/big"
+	"context"
+	"time"
 
-	"github.com/go-resty/resty/v2"
-	"github.com/lestrrat-go/jwx/v3/jwa"
+	"github.com/lestrrat-go/httprc/v3"
+	"github.com/lestrrat-go/jwx/v3/jwk"
 	"github.com/lestrrat-go/jwx/v3/jwt"
 )
 
-var pubkey *rsa.PublicKey
-
-type jwk struct {
+type jwkData struct {
 	Kty string `json:"kty"`
 	Alg string `json:"alg"`
 	Use string `json:"use"`
@@ -30,57 +25,46 @@ type User struct {
 	Avatar string
 }
 
-// initialize
-func Init(address string) error {
-	client := resty.New()
-	res, err := client.R().Get(address + "/jwks")
-	if err != nil {
-		return err
-	}
-
-	jwk := new(jwk)
-	if err := json.Unmarshal(res.Body(), jwk); err != nil {
-		return err
-	}
-
-	nBytes, err := base64.RawURLEncoding.DecodeString(jwk.N)
-	if err != nil {
-		return err
-	}
-	eBytes, err := base64.RawURLEncoding.DecodeString(jwk.E)
-	if err != nil {
-		return err
-	}
-
-	n := new(big.Int).SetBytes(nBytes)
-
-	// Decode exponent
-	var e int
-	for _, b := range eBytes {
-		e = e<<8 + int(b)
-	}
-
-	pubkey = &rsa.PublicKey{
-		N: n,
-		E: e,
-	}
-
-	return nil
+type jwks struct {
+	Keys []jwkData `json:"keys"`
 }
 
 // verify
-func Verify(token string) (*User, error) {
-	verified, err := jwt.Parse([]byte(token), jwt.WithKey(jwa.RS256(), pubkey))
+func Verify(address string, token string) (user *User, err error) {
+	jwkCache, err := jwk.NewCache(context.Background(), httprc.NewClient())
 	if err != nil {
-		return nil, fmt.Errorf("failed to parse/verify JWT: %w", err)
+		return
 	}
 
-	user := new(User)
-	claims := map[string]any{"user_id": user.ID, "username": user.Name, "email": user.Email, "avatar": user.Avatar}
-	for key, claim := range claims {
-		if err := verified.Get(key, claim); err != nil {
-			return nil, err
-		}
+	if err = jwkCache.Register(context.Background(), address); err != nil {
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	keyset, err := jwkCache.Refresh(ctx, address)
+	if err != nil {
+		return
+	}
+
+	verified, err := jwt.ParseString(token, jwt.WithKeySet(keyset))
+	if err != nil {
+		return
+	}
+
+	user = new(User)
+	if err := verified.Get("user_id", &user.ID); err != nil {
+		return nil, err
+	}
+	if err := verified.Get("username", &user.Name); err != nil {
+		return nil, err
+	}
+	if err := verified.Get("email", &user.Email); err != nil {
+		return nil, err
+	}
+	if err := verified.Get("avatar", &user.Avatar); err != nil {
+		return nil, err
 	}
 
 	return user, nil
